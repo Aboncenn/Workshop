96a188aeedc957694cc01236fd770182fc5ab5ad
Aurélien boncenne <aurelien.boncenne@gmail.com>
2018-11-06 14:11:14 +0100

    first commit

---
+++ db/db.php
@@ -0,0 +1,15 @@
+<?php
+// Connexion a la base de données
+$host ='localhost';
+$db_name ='';
+$user ='root';
+$password ='';
+
+try {
+$db = new PDO("mysql:dbname=" .$db_name.";host=" .$host, $user, $password, array(PDO::MYSQL_ATTR_LOCAL_INFILE=>1));
+$db ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e){
+    echo 'error while connecting :'.$e->getMessage();
+};
+?>
---
+++ db/header.php
@@ -0,0 +1,18 @@
+<?php
+/* Connexion base de donnée*/
+require('db.php');
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8;"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <meta name="description" content="">
+  <meta name="author" content="team karibou">
+  <title>Amarisk</title>
+  <!-- CSS  -->
+  <link href="../css/bootstrap.min.css" rel="stylesheet">
+  <!-- JS  -->
+  <stu src="../js/bootstrap.min.js"></script>
+</head>
+<body>

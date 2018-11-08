f0d82b23c4d4df9c48150623f914979e6614e431
Aurélien boncenne <aurelien.boncenne@gmail.com>
2018-11-08 09:36:33 +0100

    home client

--- vues/home.php
+++ vues/home.php
@@ -12,59 +12,25 @@ IF ASSUREUR
 affiche la home des assureurs
 */
 $user = $_SESSION['user'];
+$id = $_SESSION['id'];
 if($user == "1" ){ // Home client
+  var_dump($_SESSION['id']);
 ?>
 <div class="container-fluid">
   <div class="card-body">
       <h1 class="card-title text-center">Mes Biens</h1>
   </div>
   <div class="row">
+    <?php
+    $req = $db->prepare("SELECT * FROM contrat WHERE id_users = '$id'");
+    $req->execute();
+    ?>
       <div class="col-4 bien">
           <div class="card">
               <a href="">
                   <div class="card-body text-center">
                       <h6>Voiture :</h6>
                       <img class="bien-icn" src="../img/car.svg" />
-                  </div>
-              </a>
-          </div>
-      </div>
-      <div class="col-4 bien">
-          <div class="card">
-              <a href="">
-                  <div class="card-body text-center">
-                      <h6>Voiture :</h6>
-                      <img class="bien-icn" src="../img/car.svg" />
-                  </div>
-              </a>
-          </div>
-      </div>
-      <div class="col-4 bien">
-          <div class="card">
-              <a href="">
-                  <div class="card-body text-center">
-                      <h6>Voiture :</h6>
-                      <img class="bien-icn" src="../img/car.svg" />
-                  </div>
-              </a>
-          </div>
-      </div>
-      <div class="col-4 bien">
-          <div class="card">
-              <a href="">
-                  <div class="card-body text-center">
-                      <h6>Voiture :</h6>
-                      <img class="bien-icn" src="../img/car.svg" />
-                  </div>
-              </a>
-          </div>
-      </div>
-      <div class="col-4 bien">
-          <div class="card">
-              <a href="">
-                  <div class="card-body text-center">
-                      <h6>Ajouter un bien</h6>
-                      <img class="bien-icn" src="../img/plus.svg" />
                   </div>
               </a>
           </div>

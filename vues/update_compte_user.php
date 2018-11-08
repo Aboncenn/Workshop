4468b3f1d5fa6e236b95d9be17c9681ad41897e7
Nicolas <nicolas@bouyssounouse.fr>
2018-11-08 13:59:23 +0100

    centre du contenus de la page 'modifier compte'

--- vues/update_compte_user.php
+++ vues/update_compte_user.php
@@ -1,5 +1,7 @@
 <?php
 require('../db/header.php');
+require('../db/nav.php');
+
 
 $user = $_SESSION['user'];
 $id = $_SESSION['id'];
@@ -15,28 +17,40 @@ while ($donnees = $req->fetch())
   $tel = $donnees['telephone'];
 }
 ?>
+<h1 class="text-center">Modification de compte</h1>
 <div class="container-fluid">
-  <form method="post" action="../controller/update_account_db.php" role="create_account_db">
-    <div class="form-group">
-      <label for="exampleNom">Nom</label>
-      <input type="nom" name="nom" class="form-control" id="exampleNom" aria-describedby="nom" placeholder="Saisir le nom" value="<?php echo $nom?>">
+  <div class="row">
+    <div class="col-4"></div>
+    <div class="col-4">
+      <form method="post" action="../controller/update_account_db.php" role="create_account_db">
+        <div class="form-group">
+          <label for="exampleNom">Nom</label>
+          <input type="nom" name="nom" class="form-control" id="exampleNom" aria-describedby="nom" placeholder="Saisir le nom"
+            value="<?php echo $nom?>">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPrenom">Prénom</label>
+          <input type="prenom" name="prenom" class="form-control" id="exampleInputPrenom" aria-describedby="prenom"
+            placeholder="Saisir le prénom" value="<?php echo $prenom?>">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputTelephone">Téléphone</label>
+          <input type="telephone" name="telephone" class="form-control" id="exampleInputTelephone" aria-describedby="prenom"
+            placeholder="Saisir le téléphone" value="<?php echo $tel?>">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Adresse mail</label>
+          <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+            placeholder="Saisir l'adresse mail" value="<?php echo $mail?>">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Mot de passe</label>
+          <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Saisir le mot de passe"
+            value="<?php echo $mdp?>">
+        </div>
+        <button type="submit" class="btn btn-primary">Mettre à jour</button>
+      </form>
     </div>
-    <div class="form-group">
-      <label for="exampleInputPrenom">Prénom</label>
-      <input type="prenom" name="prenom" class="form-control" id="exampleInputPrenom" aria-describedby="prenom" placeholder="Saisir le prénom" value="<?php echo $prenom?>">
-    </div>
-    <div class="form-group">
-      <label for="exampleInputTelephone">Téléphone</label>
-      <input type="telephone" name="telephone" class="form-control" id="exampleInputTelephone" aria-describedby="prenom" placeholder="Saisir le téléphone" value="<?php echo $tel?>">
-    </div>
-    <div class="form-group">
-      <label for="exampleInputEmail1">Adresse mail</label>
-      <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Saisir l'adresse mail" value="<?php echo $mail?>">
-    </div>
-    <div class="form-group">
-      <label for="exampleInputPassword1">Mot de passe</label>
-      <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Saisir le mot de passe" value="<?php echo $mdp?>">
-    </div>
-    <button type="submit" class="btn btn-primary">Mettre à jour</button>
-  </form>
+    <div class="col-4"></div>
+  </div>
 </div>
